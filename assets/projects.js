4518cf4d86bce1875ee8b07b92eda5cf6e9e0366
Galatia <ptrivedius@aol.com>
2021-08-13 10:46:05 -0400

    URL for Rotten Trailers in projects.js

--- assets/projects.js
+++ assets/projects.js
@@ -1,4 +1,17 @@
 var requestUrl = 'https://api.github.com/users/ptriv1/repos';
+var rottenUrl = 'https://api.github.com/users/jeffcela/repos';
+
+function getProject() {
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data)
+    }
+  );
+ 
+}
 
 function getApi() {
     fetch(requestUrl)
@@ -11,5 +24,7 @@ function getApi() {
     );
   
 }
+
+getProject();
 
 getApi();
